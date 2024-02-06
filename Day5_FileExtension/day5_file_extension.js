91f557f0f6ff60320ee93a9f4684cc4956928c84
MOHSIN KHAN <87376173+Mohsinkkk@users.noreply.github.com>
2024-02-06 20:15:00 +0530

    Add files via upload

--- Day5_FileExtension/day5_file_extension.js
+++ Day5_FileExtension/day5_file_extension.js
@@ -1,1 +1,15 @@
+const path = require('path');
 
+function checkFileExtension(filePath, expectedExtension) {
+    const actualExtension = path.extname(filePath);
+
+    if (actualExtension === expectedExtension) {
+        console.log(`File has the expected extension: ${expectedExtension}`);
+    } else {
+        console.log(`File does not have the expected extension. Expected: ${expectedExtension}, Actual: ${actualExtension}`);
+    }
+}
+
+checkFileExtension('../Day4-ResolvePath/day4_resolvepath.js', '.txt');
+
+checkFileExtension('../Day4-ResolvePath/day4_resolvepath.md', '.md');
